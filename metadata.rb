64a093806db7c57f215ba18169d2c3fc08d9cfb4
Bernie Durfee <bernie.durfee@ge.com>
2015-12-09 17:55:49 -0500

    Travis-CI updates for Supermarket push

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,15 @@
-name    'xldeploy'
+name 'xldeploy'
 version '0.1.3'
+
+description 'Installs and configures XL Deploy from XebiaLabs'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
+supports 'centos'
+
+license 'Apache v2.0'
+
+source_url 'https://github.com/electric-it/xldeploy'
+issues_url 'https://github.com/electric-it/xldeploy/issues'
 
 depends 'runit',  '= 1.5.10'
 depends 'java',   '= 1.25.0'
