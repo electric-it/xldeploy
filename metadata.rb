8a45833903a9a76fe80d7010ccf7f6468dc55187
Bernie Durfee <bernie.durfee@ge.com>
2015-12-09 16:56:11 -0500

    Travis-CI updates for Supermarket push

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name    'xldeploy'
-version '0.1.0'
+version '0.1.1'
 
-depends 'runit',        '= 1.5.10'
-depends 'java',         '= 1.25.0'
-depends 'tar', 			'= 0.5.0'
+depends 'runit',  '= 1.5.10'
+depends 'java',   '= 1.25.0'
+depends 'tar',    '= 0.5.0'
