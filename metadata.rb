73ad63e8016cecb18ec8fc8cb009a06b599525b5
Bernie Durfee <bernie.durfee@ge.com>
2017-12-19 14:03:17 -0500

    Deployment

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name    'xldeploy'
-version '1.0.1'
+version '1.0.2'
 
 description 'Installs and configures XL Deploy from XebiaLabs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
@@ -8,8 +8,9 @@ supports 'centos'
 
 license 'Apache v2.0'
 
-source_url 'https://github.com/electric-it/xldeploy'
-issues_url 'https://github.com/electric-it/xldeploy/issues'
+# Check for resolution on (https://github.com/travis-ci/dpl/issues/464) before uncommenting
+# source_url 'https://github.com/electric-it/xldeploy'
+# issues_url 'https://github.com/electric-it/xldeploy/issues'
 
 depends 'runit',  '~> 1.5'
 depends 'java',   '~> 1.25'
